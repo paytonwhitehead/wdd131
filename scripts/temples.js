93a4abc51c45b3ec1651d34b18dc7c591c3a2dba
paytonwhitehead <paytonzmail@gmail.com>
2024-09-22 22:06:51 -0600

    start-of-temple.html -and-fix-to-query-styles.

---
+++ scripts/temples.js
@@ -0,0 +1,6 @@
+const date = new Date();
+const year = date.getFullYear();
+document.getElementById('currentyear').innerHTML = year;
+
+let text = document.lastModified;
+document.getElementById("lastmod").innerHTML = text;
